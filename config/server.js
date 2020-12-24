0c56b3f6bf5d1a85496ba038290412e3d484aaf2
DigvijayDheer <digvijaydheersaini@gmail.com>
2020-12-24 17:38:49 +0530

    add documentations

---
+++ config/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+    APPLICATION_ID: process.env.APPLICATION_ID || 'SKoqrmKsemqjd6j_LkNl3mfSgxLWywsJCFw-T_YEbRw',
+    SECRET: process.env.SECRET || 'cqjlsumvkG3YXjpxCWw80vwi-9PS0qQ0A3fVhN049hY',
+    CALLBACK_URL = process.env.CALLBACK_URL || 'http://localhost:3000'
+};
---
+++ server.js
@@ -0,0 +1,11 @@
+global.fetch = require('node-fetch');
+const config = require('universal-config');
+const Unsplash = require('unsplash').default;
+const toJson = require('unsplash').toJson;
+const express = require('express');
+
+const unsplash = new Unsplash({
+    applicationId: config.get('APPLICATION_ID'),
+    secret: config.get('SECRET'),
+    callbackUrl: config.get('CALLBACK_URL')
+});
